0ebc5e20ec572fc8e883e4cfa2a78faa7a9a2e93
Andre Victor <andrevictor2102@hotmail.com>
2025-07-07 15:32:55 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
@@ -1,18 +1,16 @@
 package com.nelioalves.workshopmongo.resources;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController //identifica que a classe é um controller
 @RequestMapping(value = "/users") // caminho do endpoint
@@ -22,11 +20,15 @@ public class UserResources {
     private UserService userService;
 
     @GetMapping // pra informar que esse é um endpoint rest do tipo GET
-    public ResponseEntity<List<User>> findAll() { // response entity serve para retornar os cabeçalhos certinho e possiveis erros
+    public ResponseEntity<List<UserDTO>> findAll() { // response entity serve para retornar os cabeçalhos certinho e possiveis erros
         List<User> users = userService.findAll();// assim eu uso o meu metodo que esta na classe de serviço pra a minha lista de users no meu banco de dados atraves da repository
-        for (User user : users) {
-            System.out.println(user);
+
+        List<UserDTO> listDtos = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+
+        for(UserDTO dto : listDtos) {
+            System.out.println(dto.toString());
         }
-        return ResponseEntity.ok().body(users); // retornando response entity resposta ok e o corpo d aresposta é a lista de users
+
+        return ResponseEntity.ok().body(listDtos); // retornando response entity resposta ok e o corpo d aresposta é a lista de users
     }
 }
